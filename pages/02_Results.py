4a98d5cb5cbe162304ec52fbff9dcc9c042b130c
Johan Santos <jrasantos22@gmail.com>
2024-05-23 18:59:15 +0800

    Update 02_Results.py

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title("Results")
 st.write("Here you can put each of your key results.")
+
+st.header("Top Customer Spending")
+st.image("../images/total_spending.png")
+st.write("Total Spending")
