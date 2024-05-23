19ae0ff73f7f486e1f0e2a06ccb3e20d2b1d6989
Johan Santos <jrasantos22@gmail.com>
2024-05-23 19:09:48 +0800

    Update 02_Results.py

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("Results")
 st.write("Here you can put each of your key results.")
 
-st.header("Top Customer Spending")
-st.image("../project-demo/images/total_spending.png")
-st.write("Total Spending")
+st.header("1. Top Customer spending behavior by category")
+st.image("project-demo/images/total_spending.png")
+st.write("Travel is the top spender's highest spending, followed by onsite grocery and onsite shopping")
