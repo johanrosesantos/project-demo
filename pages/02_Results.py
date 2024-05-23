f51403e1f741f96ec7f35b1752da8e529baf7d22
Johan Santos <jrasantos22@gmail.com>
2024-05-23 19:10:42 +0800

    Update 02_Results.py

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -4,5 +4,5 @@ st.title("Results")
 st.write("Here you can put each of your key results.")
 
 st.header("1. Top Customer spending behavior by category")
-st.image("project-demo/images/total_spending.png")
+st.image("images/total_spending.png")
 st.write("Travel is the top spender's highest spending, followed by onsite grocery and onsite shopping")
